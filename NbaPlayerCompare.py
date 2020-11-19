e806ee78abeeea82fbb38907f146340312af70cf
Qbiwan <Qbiwan1934@yahoo.com>
2020-11-19 21:13:46 +0800

    first commit

---
+++ NbaPlayerCompare.py
@@ -0,0 +1,57 @@
+import config
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.set_page_config(layout="wide")
+
+col1 = st.sidebar
+col2, col3 = st.beta_columns([3, 1])
+col2.title("Individual NBA Player Statistics")
+
+with col1:
+    st.subheader("User Input Features")
+    selected_year = st.selectbox("Year 1 to Year", list((range(2, 9))), 3)
+    SelectedPlayer = st.multiselect("Player",
+                                    config.PLAYER_LIST,
+                                    config.PLAYER_LIST[:5]
+                                    )                              
+
+with col2:
+    @st.cache(allow_output_mutation=True)
+    def load_data(url):
+        html = pd.read_html(url, header=0)
+        df = html[0]
+        df = df[:selected_year]
+        df["Years In NBA"] = range(1, selected_year+1)
+        return df
+
+    dataframes = []
+    for player in SelectedPlayer:
+        st.title(player)
+        url = config.URL+config.URL_SUFFIX[player]
+        df = load_data(url)
+        df["Player Name"] = player
+        dataframes.append(df)
+        st.write(df)
+
+cols = list(dataframes[0].columns[5:-2])
+selected_stats = st.sidebar.multiselect('Player Stats', cols[5:-2], cols[5:10])
+col1.markdown(config.GLOSSARY)
+
+dataframes_stacked = pd.concat(dataframes, axis=0)
+dataframes_stacked.to_csv('output.csv', index=False)
+dataframes_stacked = pd.read_csv('output.csv')
+
+col3.title("Player Comparison")
+with col3:
+    for stat in selected_stats:
+        st.subheader(config.GLOSSARY2[stat])
+        pivot = dataframes_stacked.pivot("Years In NBA", "Player Name", stat)
+        st.write(pivot)
+        with sns.axes_style("darkgrid"):
+            f, ax = plt.subplots(figsize=(7, 5))
+            ax = sns.lineplot(data=pivot)
+            st.pyplot(f)
---
+++ config.py
@@ -0,0 +1,83 @@
+URL = "https://www.basketball-reference.com/players/"
+
+PLAYER_LIST = ["Michael Jordan",
+               "Lebron James",
+               "Kobe Bryant",
+               "Larry Bird",
+               "Magic Johnson",
+               "Hakeem Olajuwon",
+               "Kareem Abdul-Jabbar",
+               "Shaquille O'Neal",
+               "Wilt Chamberlain",
+               "Bill Russell"
+               ]
+
+URL_SUFFIX = {"Michael Jordan": "/j/jordami01.html",
+              "Lebron James": "j/jamesle01.html",
+              "Kobe Bryant": "b/bryanko01.html",
+              "Larry Bird": "b/birdla01.html",
+              "Magic Johnson": "j/johnsma02.html",
+              "Hakeem Olajuwon": "o/olajuha01.html",
+              "Kareem Abdul-Jabbar":"a/abdulka01.html",
+              "Shaquille O'Neal": "o/onealsh01.html",
+              "Wilt Chamberlain": "c/chambwi01.html",
+              "Bill Russell": "r/russebi01.html",
+              }
+
+GLOSSARY = ("""
+## Glossary  
+  
+**G**   : Games  
+**GS**  : Games Started  
+**MP**  : Minutes Played Per Game  
+**FG**  : Field Goals Per Game  
+**FGA** : Field Goal Attempts Per Game  
+**FG%** : Field Goal Percentage  
+**3P**  : 3-Point Field Goals Per Game  
+**3PA** : 3-Point Field Goal Attempts Per Game  
+**3P%** : 3-Point Field Goal Percentage  
+**2P**  : 2-Point Field Goals Per Game  
+**2PA** : 2-Point Field Goal Attempts Per Game  
+**2P%** : 2-Point Field Goal Percentage  
+**eFG%**: Effective Field Goal Percentage  
+**FT**  : Free Throws Per Game  
+**FTA** : Free Throw Attempts Per Game  
+**FT%** : Free Throw Percentage  
+**ORB** : Offensive Rebounds Per Game  
+**DRB** : Defensive Rebounds Per Game  
+**TRB** : Total Rebounds Per Game  
+**AST** : Assists Per Game  
+**STL** : Steals Per Game  
+**BLK** : Blocks Per Game  
+**TOV** : Turnovers Per Game  
+**PF**  : Personal Fouls Per Game  
+**PTS** : Points Per Game  
+""")
+
+
+GLOSSARY2 = {"G": "Games",
+             "GS": "Games Started",
+             "MP": "Minutes Played Per Game",
+             "FG": "Field Goals Per Game",
+             "FGA": "Field Goal Attempts Per Game",
+             "FG%": "Field Goal Percentage",
+             "3P": "3-Point Field Goals Per Game",
+             "3PA": "3-Point Field Goal Attempts Per Game", 
+             "3P%": "3-Point Field Goal Percentage",
+             "2P": "2-Point Field Goals Per Game",
+             "2PA": "2-Point Field Goal Attempts Per Game",
+             "2P%": "2-Point Field Goal Percentage",
+             "eFG%": "Effective Field Goal Percentage",
+             "FT": "Free Throws Per Game",
+             "FTA": "Free Throw Attempts Per Game",
+             "FT%": "Free Throw Percentage",
+             "ORB": "Offensive Rebounds Per Game",
+             "DRB": "Defensive Rebounds Per Game",
+             "TRB": "Total Rebounds Per Game",
+             "AST": "Assists Per Game",
+             "STL": "Steals Per Game",
+             "BLK": "Blocks Per Game",
+             "TOV": "Turnovers Per Game",
+             "PF": "Personal Fouls Per Game",
+             "PTS": "Points Per Game",
+             }
